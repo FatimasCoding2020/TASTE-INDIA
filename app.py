2ea4b222585cd6b07b86a3a4b59ed05a1df7d352
Fatima Omer <fatimasmails@gmail.com>
2021-04-22 00:32:06 +0100

    connect to flask and create home page

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import (
+    Flask,
+)
+
+import os
+
+
+
+app = Flask(__name__)
+
+
+
+
+
+
+
+
+
+@app.route("/", methods=["GET"])
+def home():
+    
+
+    return "Hello there"
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
