63904feedad4ceb7272606ef734180f497f3ab62
Fatima Omer <fatimasmails@gmail.com>
2021-04-23 12:26:21 +0100

    get data from database for the home page

--- app.py
+++ app.py
@@ -2,29 +2,36 @@ import os
 from flask import (
     Flask,
     render_template,
+    request,
 )
-
 from database import db
-
+from controllers.login_authorize import login_authorize
+from controllers.recipe_controller import *
 
 
 app = Flask(__name__)
-
-
 
 app.secret_key = os.getenv("SECRET_KEY")
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 IMAGE_DIR = os.path.join(APP_ROOT, "static/uploaded_images")
 
 
-
-
-
 @app.route("/", methods=["GET"])
 def home():
-    
+    try:
+        print("_______________________________", request.host_url)
+        login_data = login_authorize(request, db)
+        # if login is unsuccessfull redirecting to login page again
+        islogin = True if login_data["success"] else False
+        response = all_recipe_controller_home(
+            {}, db_conn=db, host_url=request.host_url)
+        return render_template("index.html", result=response, islogin=islogin)
 
-    return render_template("index.html")
+    except BaseException:
+
+        return render_template("error.html")
+
+
 
 
 
---
+++ controllers/recipe_controller.py
@@ -0,0 +1,20 @@
+import os
+from bson import json_util, ObjectId
+import json
+
+
+
+# function to query all recipe from database
+
+def all_recipe_controller_home(filters, db_conn, host_url):
+    print("from all recipe...")
+    print("filter :", filters)
+    try:
+        result = db_conn["recipes"].find(
+            filters).sort("createdOn", -1).limit(9)
+        recipe_list = map_response(result, host_url)
+        return recipe_list
+    except Exception as e:
+        print(e)
+        return {"success": False, "message": "Error in api: " + str(e)}
+
