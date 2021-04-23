cf596456152c9a06928e454e55a909204be84359
Fatima Omer <fatimasmails@gmail.com>
2021-04-23 13:09:21 +0100

    implement signup page

--- app.py
+++ app.py
@@ -3,11 +3,14 @@ from flask import (
     Flask,
     render_template,
     request,
+    redirect,
+    url_for,
 )
 from database import db
 from controllers.login_authorize import login_authorize
 from controllers.recipe_controller import *
-
+from controllers.users_controller import *
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -29,12 +32,37 @@ def home():
 
     except BaseException:
 
-        return render_template("error.html")
+        return render_template("error_handlers/error.html")
 
 
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
+    try:
+        if request.method == "POST":
+            checkbox = (request.form.get("checkbox")
+                        if "checkbox" in request.form else "off")
 
+            if checkbox == "off":
+                error = "Please select terms & condition and proceed"
+                return render_template("users/signup.html", error=error)
 
+            payload = dict(
+                email=request.form["email"],
+                username=request.form["username"],
+                password=request.form["password"],
+                creadedOn=datetime.now(),
+            )
+            response = signup_controller(payload=payload, db_conn=db)
+            print("response----------------------------", response)
+            if response["success"]:
+                return redirect(url_for("login"))
+            else:
+                error = "User already exists"
+                return render_template("users/signup.html", error=error)
 
+        return render_template("users/signup.html")
+    except BaseException:
+        return render_template("error_handlers/error.html")
 
 
 if __name__ == "__main__":
---
+++ controllers/users_controller.py
@@ -0,0 +1,31 @@
+from pprint import pprint
+from bson import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
+
+
+# user signup
+def signup_controller(payload, db_conn):
+    print("Inside signup_controller")
+    try:
+        # validate email if it already exists
+        email_check = db_conn["users"].find_one({"email": payload["email"]})
+
+        if email_check is None:
+            password = payload["password"]
+            payload["password"] = generate_password_hash(password)
+            # role mapping
+            pprint(payload)
+            # insert the user
+            result = db_conn["users"].insert_one(payload)
+            print(result.inserted_id)
+            print(result.acknowledged)
+            return {
+                "success": True,
+                "message": "User created successfully",
+                "_id": result.inserted_id,
+            }
+        else:
+            return {"success": False, "message": "User already exists"}
+    except Exception as e:
+        return {"success": False, "message": "Error in api: " + str(e)}
+
